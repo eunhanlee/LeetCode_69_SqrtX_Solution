83f6282ec84208a37782c3eb1cf756a45177f89e
eunhanlee58 <eunhanlee58@gmail.com>
2023-08-24 13:23:58 -0400

    add javadoc

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,6 +1,13 @@
 package org.example;
 
-class Solution {
+
+public class Solution {
+    /**
+     * Calculates and returns the integer square root of the given non-negative integer x.
+     *
+     * @param x The non-negative integer x for which to calculate the square root.
+     * @return The largest integer whose square is less than or equal to x.
+     */
     public int mySqrt(int x) {
         return (int) Math.sqrt(x);
     }
